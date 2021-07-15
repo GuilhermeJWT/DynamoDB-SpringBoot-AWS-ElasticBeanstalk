5ccdf5a27652f3ea05af62e53b878d4e927a2939
Guilherme Santos <guiromanno@gmail.com>
2021-07-15 13:39:07 -0300

    Definindo Anotações do Dynamo para os Models e Atributos

--- src/main/java/br/com/systemsgs/dynamodb/model/ModelEndereco.java
+++ src/main/java/br/com/systemsgs/dynamodb/model/ModelEndereco.java
@@ -1,12 +1,29 @@
 package br.com.systemsgs.dynamodb.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@DynamoDBDocument
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ModelEndereco implements Serializable {
 
+    @DynamoDBAttribute
     private String cidade;
+
+    @DynamoDBAttribute
     private String uf;
+
+    @DynamoDBAttribute
     private String cep;
+
+    @DynamoDBAttribute
     private String logradouro;
 
 }
